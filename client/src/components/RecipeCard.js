0309feede3f3a46b086bc93bed68b5f8abd7e982
Debra Sparr <50783303+dsparr1010@users.noreply.github.com>
2020-03-26 17:17:35 -0500

    add card component

---
+++ client/src/components/RecipeCard.js
@@ -0,0 +1,12 @@
+import React, { useEffect, useState } from "react";
+import { Col, Jumbotron, Container, Button, Form, Card } from 'react-bootstrap';
+
+const RecipeCard = () => {
+    
+
+    return (
+
+    );
+};
+
+export default RecipeCard;
